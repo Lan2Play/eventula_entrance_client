fa47e261c6a1bfc2a5b9ed18bd54c8b5b76a0b5d
Alexander Volz <Alexander@volzit.de>
2023-03-24 18:26:04 +0100

    moved to imagesharp

--- Services/ZXingBarcodeService.cs
+++ Services/ZXingBarcodeService.cs
@@ -1,13 +1,16 @@
 ﻿using EventulaEntranceClient.Services.Interfaces;
-using System.Drawing;
 
-using ZXing.Windows.Compatibility;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+// using ZXing.Windows.Compatibility;
+// using ZXing;
+using ZXing.ImageSharp;
 
 namespace EventulaEntranceClient.Services;
 
 public class ZXingBarcodeService : IBarcodeService
 {
-    private readonly BarcodeReader _BarcodeReader = new BarcodeReader();
+    private readonly BarcodeReader<Rgb24> _BarcodeReader = new BarcodeReader<Rgb24>();
     private readonly ILogger<ZXingBarcodeService> _Logger;
 
     public ZXingBarcodeService(ILogger<ZXingBarcodeService> logger)
@@ -21,14 +24,20 @@ public class ZXingBarcodeService : IBarcodeService
         {
             using (var ms = new MemoryStream(image))
             {
-                var img = (Bitmap)Image.FromStream(ms);
-
-                var result = _BarcodeReader.Decode(img);
-                // do something with the result
-                if (result != null)
+                using (var img = (Image<Rgb24>)Image.Load(ms)) 
                 {
-                    return result.Text;
+                    var result = _BarcodeReader.Decode(img);
+                    // do something with the result
+                    if (result != null)
+                    {
+                        return result.Text;
+                    }
                 }
+
+                // var img = Image<Rgba32>.(ms);
+
+
+               
             }
         }
         catch(Exception ex)
